8c978c6c87eca9036efc8c243955792fb1674cc2
Sophia <nguyencsg@gmail.com>
2026-05-30 10:42:44 -0500

    fixed formatting of quiz with home landing page and quiz

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
+
+@app.route("/", methods=["GET", "POST"])
+def quiz():
+
+    if request.method == "POST":
+        answer = request.form.get("answer")
+        print(answer)  # test for now
+        return redirect(url_for("quiz"))
+
+    return render_template("quiz.html")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
